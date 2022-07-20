1a5a70cfcd8cccc33c2b58f6db0239c0664b8af1
kmc369 <keishacoleman710@gmail.com>
2022-07-20 00:46:19 -0400

    routerLinksDone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FindAllComponent } from './find-all/find-all.component';
+import { PostFormComponent } from './post-form/post-form.component';
+
 
 const routes: Routes = [{
-
   path : 'find-all',
-  component : FindAllComponent,
-
+  component : FindAllComponent
+},{
+  path : 'post-form',
+  component : PostFormComponent
 }];
 
 @NgModule({
